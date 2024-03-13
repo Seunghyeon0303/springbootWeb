d744101bedf3979c1a942883b9518ea39c4862ee
user1 <user1@DESKTOP-EB7GVTQ>
2024-03-13 10:00:12 +0900

    네이버 회원가입

---
+++ 20240306/src/main/java/com/kh/join6/entity/JoinMember.java
@@ -0,0 +1,26 @@
+package com.kh.join6.entity;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class JoinMember {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String passwordConfirm;
+    private String name;
+    private String birthYear;
+    private String birthMonth;
+    private String birthDay;
+    private String gender;
+    private String phoneNumber;
+}
