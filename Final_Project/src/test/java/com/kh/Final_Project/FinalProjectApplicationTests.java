02353f6f20e268238cb96fb31fe4c4348ba9093f
pc <pc@192.168.123.101>
2024-04-02 17:20:36 +0900

    Final Project

---
+++ Final_Project/src/test/java/com/kh/Final_Project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
